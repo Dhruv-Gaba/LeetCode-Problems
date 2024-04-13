d2bb3ad09b1639360034b2c7305c42db34db46c3
Dhruv-Gaba <141745373+Dhruv-Gaba@users.noreply.github.com>
2024-04-13 23:24:53 +0530

    Create solution.java

---
+++ Trapping Rain Water/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int trap(int[] height) {
+        int leftmax[]=new int[height.length];
+        leftmax[0]=height[0];
+        for(int i=1;i<height.length;i++){
+            leftmax[i]=Math.max(height[i],leftmax[i-1]);
+        }
+        int rightmax[]=new int[height.length];
+        rightmax[height.length-1]=height[height.length-1];
+        for(int i=height.length-2;i>=0;i--){
+            rightmax[i]=Math.max(height[i],rightmax[i+1]);
+        }
+        int waterlevel;
+        int totallevel=0;
+        for(int i=0;i<height.length;i++){
+            waterlevel=(Math.min(leftmax[i],rightmax[i]));
+            totallevel+=(waterlevel-height[i]);
+        }
+        return totallevel;
+    }
+}
