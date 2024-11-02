aac13abcaebfdd913c4d5f401dbf631c86a3c267
Dhruv-Gaba <141745373+Dhruv-Gaba@users.noreply.github.com>
2024-11-02 20:56:59 +0530

    Create solution.java

---
+++ 2490. Circular Sentence/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isCircularSentence(String sentence) {
+        int n=sentence.length();
+        if(sentence.charAt(0)!=sentence.charAt(n-1)){
+            return false;
+        }
+        for(int i=1;i<n;i++){
+            if(sentence.charAt(i)==' '){
+                if(sentence.charAt(i-1)!=sentence.charAt(i+1)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
