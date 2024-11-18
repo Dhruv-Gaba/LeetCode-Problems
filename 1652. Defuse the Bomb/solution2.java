7d1208e6ae502577100e51066b3dfa2e416df402
Dhruv-Gaba <141745373+Dhruv-Gaba@users.noreply.github.com>
2024-11-18 13:33:28 +0530

    Create solution2.java

---
+++ 1652. Defuse the Bomb/solution2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int n=code.length;
+        int ans[]=new int[n];
+        if(k==0){
+            return ans;
+        }
+        for(int i=0;i<n;i++){
+            if(k>0){
+                for(int j=i+1;j<=i+k;j++){
+                    ans[i]+=code[j%n];
+                }
+            }
+            else{
+                for(int j=i-1;j>=i+k;j--){
+                    ans[i]+=code[(j+n)%n];
+                }
+            }
+        }
+        return ans;
+    }
+}
