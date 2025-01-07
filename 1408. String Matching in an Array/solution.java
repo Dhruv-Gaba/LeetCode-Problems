7af5abb7aef3f0af3682436b9d8ebf44119c03c2
Dhruv-Gaba <141745373+Dhruv-Gaba@users.noreply.github.com>
2025-01-07 21:48:49 +0530

    Create solution.java

---
+++ 1408. String Matching in an Array/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<String> stringMatching(String[] words) {
+        List<String> list=new ArrayList<>();
+        for(int i=0;i<words.length;i++){
+            for(int j=0;j<words.length;j++){
+                if(i==j){
+                    continue;
+                }
+                if(words[j].contains(words[i])){
+                    list.add(words[i]);
+                    break;
+                }
+            }
+        }
+        return list;
+    }
+}
