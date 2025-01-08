522a1b6685d0c9f3db24793ecdf3c482521c05aa
Dhruv-Gaba <141745373+Dhruv-Gaba@users.noreply.github.com>
2025-01-09 00:08:42 +0530

    Create solution.java

---
+++ 3042. Count Prefix and Suffix Pairs I/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countPrefixSuffixPairs(String[] words) {
+        int count=0;
+        for(int i=0;i<words.length;i++){
+            for(int j=i+1;j<words.length;j++){
+                if(words[j].startsWith(words[i]) && words[j].endsWith(words[i])){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
