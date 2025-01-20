31f411e7602f72e61d1f1551c0f7ce3ecec8e2ac
Dhruv-Gaba <141745373+Dhruv-Gaba@users.noreply.github.com>
2025-01-21 02:19:36 +0530

    Update solution.java

--- 303. Range Sum Query - Immutable/solution.java
+++ 303. Range Sum Query - Immutable/solution.java
@@ -1,26 +1,27 @@
-class Solution {
-    public int firstCompleteIndex(int[] arr, int[][] mat) {
-        int m=mat.length;
-        int n=mat[0].length;
-        int[] rows=new int[m];
-        int[] cols=new int[n];
-        Map<Integer,int[]> map=new HashMap<>();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                map.put(mat[i][j],new int[]{i,j});
-            }
+class NumArray {
+    int nums[];
+    int prefixSum[];
+    public NumArray(int[] nums) {
+        this.nums=nums;
+        prefixSum=new int[nums.length];
+        prefixSum[0]=nums[0];
+        for(int i=1;i<nums.length;i++){
+            prefixSum[i]+=prefixSum[i-1]+nums[i];
         }
-        for(int i=0;i<m*n;i++){
-            int idx[]=map.get(arr[i]);
-            rows[idx[0]]++;
-            cols[idx[1]]++;
-            if(rows[idx[0]]==n){
-                return i;
-            }
-            if(cols[idx[1]]==m){
-                return i;
-            }
+    }
+    
+    public int sumRange(int left, int right) {
+        if(left==0){
+            return prefixSum[right];
         }
-        return -1;
+        else{
+            return prefixSum[right]-prefixSum[left-1];
+        }
     }
 }
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
