99f7ab19c801534ba789a2233a7b7ff8debaebb3
Dhruv-Gaba <141745373+Dhruv-Gaba@users.noreply.github.com>
2025-03-19 13:25:31 +0530

    Create solution.java

---
+++ 3191. Minimum Operations to Make Binary Array Elements Equal to One I/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int count=0;
+        int n=nums.length;
+        for(int i=0;i<n-2;i++){
+            if(nums[i]==0){
+                nums[i]=1;
+                nums[i+1]=nums[i+1]^1;
+                nums[i+2]=nums[i+2]^1;
+                count++;
+            }
+        }
+        return nums[n-1]==1 && nums[n-2]==1?count:-1;
+    }
+}
