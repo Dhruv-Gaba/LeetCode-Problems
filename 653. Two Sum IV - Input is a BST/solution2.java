d6547a9ce6b14f905df5d196843d48f5decc0ca5
Dhruv-Gaba <141745373+Dhruv-Gaba@users.noreply.github.com>
2025-04-01 15:59:25 +0530

    Create solution2.java

---
+++ 653. Two Sum IV - Input is a BST/solution2.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+        Set<Integer> set=new HashSet<>();
+        Queue<TreeNode> q=new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            TreeNode curr=q.poll();
+            if(set.contains(k-curr.val)){
+                return true;
+            }
+            set.add(curr.val);
+            if(curr.left!=null){
+                q.add(curr.left);
+            }
+            if(curr.right!=null){
+                q.add(curr.right);
+            }
+        }
+        return false;
+    }
+}
